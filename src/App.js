42b109acb63df37e04431599acadb149a530fa6d
krishna-Sathish <romansathish3337@gmail.com>
2024-11-17 22:36:02 +0530

    Contact section created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './Components/Hero'
 import About from './Components/About'
 import Projects from './Components/Projects'
 import Resume from './Components/Resume'
+import Contact from './Components/Contact'
 const App = () => {
   return (
     <div>
@@ -13,6 +14,7 @@ const App = () => {
         <About/>
         <Projects/>
         <Resume/>
+        <Contact/>
     </div>
   )
 }
---
+++ src/Components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const Contact= () => {
+  return (
+    <section className=' flex flex-col  bg-gray-800 px-5 py-32'>
+             
+            <div className=' flex flex-col items-center text-white'>
+                 <h1 className='text-white text-4xl border-b-4  mb-5 w-[140px] font-bold'>Contact</h1> 
+                <p >Feel free to contact me for a more detailed discussion.</p>
+                 <p className='py-2'><span className='font-bold'>Email :</span> romansathish3337@gmail.com</p>
+                 <p className='py-2'><span className='font-bold'>Phone :</span> 9025464168</p>
+                
+        </div> 
+    </section>
+  )
+}
+
+export default Contact ;
